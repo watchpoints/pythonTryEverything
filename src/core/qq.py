949259fd4c14c9bcd1918e755b9ce5add8370cf9
程序员小王(troy) troy <wang_cyi@163.com>
2023-07-01 10:52:43 +0000

    emal

--- src/core/qq.py
+++ src/core/qq.py
@@ -6,11 +6,11 @@ from email.utils import encode_rfc2231
 
 # 第三方 SMTP 服务
 mail_host = "smtp.qq.com"      # SMTP服务器
-mail_user = "344858964@qq.com"         # 用户名
+mail_user = "@qq.com"         # 用户名
 mail_pass = "eyjhsujpzogibhaj"         # 授权密码，非登录密码
  
-sender ="344858964@qq.com"   # 发件人邮箱(最好写全, 不然会失败)
-receivers = ['344858964@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender ="@qq.com"   # 发件人邮箱(最好写全, 不然会失败)
+receivers = ['@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 
 title = 'tell'  # 邮件主题
