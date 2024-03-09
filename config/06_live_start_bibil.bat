eb4af4864e8074f22c3cccaf9fb2cf4d4e27951f
watchpoints <wang_cyi@163.com>
2024-03-09 16:48:43 +0800

    update

--- config/06_live_start_bibil.bat
+++ config/06_live_start_bibil.bat
@@ -1,3 +1,3 @@
 @echo off
 set PYTHONPATH=%PYTHONPATH%;D:\pythonTryEverything
-cmd /k  python D:\pythonTryEverything\auto\write\auto_write.py
+cmd /k  python D:\pythonTryEverything\auto\write\atuo_start_bibli.py
--- config/07_auto_write.bat
+++ config/07_auto_write.bat
@@ -1,3 +1,3 @@
 @echo off
 set PYTHONPATH=%PYTHONPATH%;D:\pythonTryEverything
-cmd /k  python D:\pythonTryEverything\putdonwphone\not_watch_live\bilbil\atuo_start_bibli.py
+cmd /k  python D:\pythonTryEverything\auto\write\auto_write.py
