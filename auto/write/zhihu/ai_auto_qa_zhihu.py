c1efe5101d5e33cff4f79c5105a5f2020cb2bf93
troy <wang_cyi@163.com>
2024-08-15 22:23:43 +0800

    update

--- auto/write/zhihu/ai_auto_qa_zhihu.py
+++ auto/write/zhihu/ai_auto_qa_zhihu.py
@@ -59,7 +59,7 @@ class CMyZhiHu:
     ##############################
     def get_signed_header(self)-> str:
         """
-          害怕自己的内容被第三方拿去训练大模型 乱码
+          害怕自己的内容被第三方拿去训练大模型 乱g码
         """
         # 启动浏览器
         driver = webdriver.Chrome()
@@ -80,7 +80,7 @@ class CMyZhiHu:
                 zse_ck = cookie['value']
                 break
 
-        # 打印获取到的__zse_ck参数值
+        # 打印获取到的__gzse_ck参数值
         if zse_ck:
             print("获取到的__zse_ck值为:", zse_ck)
         else:
@@ -147,30 +147,30 @@ class CMyZhiHu:
          follow ---
         """
         page.goto("https://www.zhihu.com/follow")
-        time.sleep(6)
+        page.wait_for_timeout(1000)
         print("https://www.zhihu.com/follow")
         # page.locator("xpath=//a[text()='推荐']").click()
         #page.locator("xpath=//a[contains(text(),'推荐']").click()
         page.get_by_role("main").get_by_role("link", name="推荐", exact=True).click()
-        time.sleep(4)
+        page.wait_for_timeout(1000)
         page.mouse.down()
         print("推荐")
         ## Child
         for index in [2,3,4]:
             page.mouse.down()
             page.mouse.down()
-            time.sleep(1)
+            page.wait_for_timeout(1000)
             page.mouse.down()
             page.mouse.down()
-            time.sleep(1)
+            page.wait_for_timeout(1000)
             element = page.locator("xpath=//button[contains(text(),'赞同')]").locator('nth={}'.format(index))
-            time.sleep(1)
+            page.wait_for_timeout(1000)
             print(element.all_inner_texts())
             element.hover()
-            time.sleep(3)
+            page.wait_for_timeout(3000)
             element.click()
             print("赞同完成")
-            time.sleep(3)
+            page.wait_for_timeout(3000)
         print("---------zhihu_auto_agree---------")
     ##################################自动回答#########################################   
     def zhihu_auto_answer(self, page: Page):
@@ -179,23 +179,23 @@ class CMyZhiHu:
         方向：职场
         """
         page.goto("https://www.zhihu.com/creator/featured-question/goodat-topic/all")
-        time.sleep(5)
+        page.wait_for_timeout(3000)
         page.get_by_role("link", name="擅长话题").click()
-        print("为你推荐,这个时间设置5秒，太短，改为10秒")
-        time.sleep(20)
+        print("为你推荐,这个时间设置5秒，太短，改为20秒")
+        page.wait_for_timeout(20000)
         # man question 第二个问题 下标是3
         with self.context.expect_page(timeout=20000) as new_page_info:
             #page.locator("xpath=//*[contains(text(),'写回答')]").locator("nth=1")
             #CSS选择器
             page.locator("div:nth-child(4) > .css-1hbj689 > .css-ra7giy > div > .Button").click(timeout=20000)
-        time.sleep(5)
+        page.wait_for_timeout(1000)
         question_page = new_page_info.value
         question_page.wait_for_load_state()
 
         question_title = question_page.locator("h1.QuestionHeader-title").locator("nth=1").text_content()
         print(question_title)
         
-        time.sleep(3)
+        page.wait_for_timeout(1000)
         resulut = mykimi.Get_msg_by_kimi(question_title)
         if len(resulut) == 0:
             return None
@@ -207,13 +207,13 @@ class CMyZhiHu:
         #question_page.get_by_role("main").get_by_role("button", name="​写回答").click()
         #page.locator("xpath=//button[./span[text()='发布']]").click()
         question_page.locator("xpath=//button[contains(text(),'写回答')]").locator("nth=0").click()
-        time.sleep(5)
+        page.wait_for_timeout(3000)
         
         with self.context.expect_page(timeout=20000) as page_answer1:
             #page.locator("xpath=//*[contains(text(),'写回答')]").locator("nth=1")
             #question_page.locator(".css-1codfpf").click(timeout=20000)
             question_page.get_by_text("全屏编辑").click()
-        time.sleep(5)
+        page.wait_for_timeout(3000)
         page_answer = page_answer1.value
         page_answer.wait_for_load_state()
 
@@ -221,37 +221,38 @@ class CMyZhiHu:
         # 自动发布---填写回答内容
         print("begin answer")
         page_answer.locator("css=.notranslate.public-DraftEditor-content").fill(resulut)
-        time.sleep(5)
+        page.wait_for_timeout(3000)
         
         picture_path_list = bingpic.get_random_jpg_files(self.pic_path)
         print(picture_path_list)
         if len(picture_path_list) > 0:
             print("开始上传图片")
             page_answer.get_by_role("button", name="图片").click()
-            time.sleep(3)
+            page.wait_for_timeout(3000)
             print("本地上传")
             with page_answer.expect_file_chooser() as fc_info:
                 page_answer.locator(".css-n71hcb").click()
             file_chooser = fc_info.value
             file_chooser.set_files(picture_path_list)
-            time.sleep(180) # 防止图片过大 来不及上传,3miniute
+            #time.sleep(180) # 防止图片过大 来不及上传,3miniute
+            page.wait_for_timeout(60000)
             page_answer.get_by_role("button", name="插入图片").click()
             print("insert the pic")
-            time.sleep(5)
+            page.wait_for_timeout(3000)
         
         page_answer.mouse.down()
         page_answer.mouse.down()
         page_answer.mouse.down()
         page_answer.get_by_text("无声明").click()
-        time.sleep(3)
+        page.wait_for_timeout(1000)
         page_answer.get_by_role("option", name="包含 AI 辅助创作").click()
-        time.sleep(3)
+        page.wait_for_timeout(1000)
 
         # page_answer.get_by_text("允许规范转载").click()
         # page_answer.get_by_role("option", name="禁止转载").click()
         # time.sleep(10)
         page_answer.get_by_role("button", name="发布回答").click()
-        time.sleep(10)
+        page.wait_for_timeout(5000)
         page_answer.close()
     ###################################################
     # def like_other_things(self, page: Page, user_list):
@@ -308,12 +309,12 @@ class CMyZhiHu:
         # page.locator("a").filter(has_text="发布想法").click()
         
         
-        time.sleep(2)
+        time.sleep(5)
         
         page.get_by_placeholder("请输入标题（选填）").fill(habit_name)
         page.get_by_role("textbox").locator('nth=-1').fill(habit_detail)
         # page.locator(".InputLike").fill(habit_detail)
-        time.sleep(3)
+        time.sleep(5)
         
         print("开始上传图片")
         page.locator(".css-88f71l > button:nth-child(2)").click()
@@ -323,7 +324,7 @@ class CMyZhiHu:
             page.locator(".css-n71hcb").click()
         file_chooser = fc_info.value
         file_chooser.set_files(picture_path_list)
-        time.sleep(5)
+        time.sleep(120)
        
         
         page.get_by_role("button", name="插入图片").click()
@@ -369,14 +370,8 @@ def help_ohter_by_qa():
         # 回答问题
         time.sleep(random.randint(0, 20))
         # 连续回答三个问题 这个做法不如 一次获取三个问题，每个问题继续回答
-        count = 1
-        while(count < 3):
-            try:
-                autoupload.zhihu_auto_answer(login_page)
-                time.sleep(random.randint(0, 30))
-            except Exception as mye:
-                print(mye)
-            count = count + 1
+        autoupload.zhihu_auto_answer(login_page)
+       
         autoupload.zhihu_auto_agree(login_page)
         # 关闭浏览器
         autoupload.browser.close()
