2283441bdc3d6d9375fe6011a476b45ca97d90f6
watchpoints <wang_cyi@163.com>
2024-10-06 15:44:34 +0800

    bat

--- config/12_ai_auto_zsxq.bat
+++ config/12_ai_auto_zsxq.bat
@@ -1,3 +1,3 @@
 @echo off
 set PYTHONPATH=%PYTHONPATH%;D:\pythonTryEverything
-cmd /k  python D:\pythonTryEverything\auto\write\zhihu\ai_auto_qa_zhihu_big.py
+cmd /k  python D:\pythonTryEverything\auto\write\zsxq\auto_zsxq.py
